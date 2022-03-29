ae9363ad24ea59d8869ab33ab2928ee8823e3f80
Sudheer Junnuthula <gantar.swathi@gmail.com>
2022-03-29 03:37:57 -0500

    updated SuperDemo

--- src/Abstract/SuperDemo.java
+++ src/Abstract/SuperDemo.java
@@ -3,11 +3,16 @@ package Abstract;
 public class SuperDemo {
 	public static void main(String[] args)
 			{
-		BC b;
-		b=new BC();
-		b.display();
+//	BC b;
+//		b=new BC();
+//		b.display();
+		
+		cd c;
+		c=new cd();
+		c.display();
+		c.print();
 	}
-
+	
 }
 class AB{
 	int a=70;
@@ -17,5 +22,15 @@ class BC extends AB{
 	void display() {
 		System.out.println(a);
 		System.out.println(super.a);
+		
+		
 	}
+}
+class cd extends BC{
+	int a=75;
+	void print () {
+		System.out.println(a);
+		System.out.println(super.a);
+	}
+	
 }
