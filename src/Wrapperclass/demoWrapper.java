56329e94feb4e6d789fbc66e600cbcf130b83fe0
Sudheer Junnuthula <gantar.swathi@gmail.com>
2022-03-29 03:10:59 -0500

    added

---
+++ src/Wrapperclass/demoWrapper.java
@@ -0,0 +1,15 @@
+package Wrapperclass;
+
+public class demoWrapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		Integer i1=new Integer(i);//here convert primitive to object datatype
+		System.out.println(i1);//in co
+		int i3=i1;
+		System.out.println(i3);
+		
+	}
+
+}
