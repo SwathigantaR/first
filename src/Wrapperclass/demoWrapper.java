95ef0d0c1ce10fa4fe47b471ec1c40e631e29f56
Sudheer Junnuthula <gantar.swathi@gmail.com>
2022-04-05 00:55:20 -0500

    update

--- src/Wrapperclass/demoWrapper.java
+++ src/Wrapperclass/demoWrapper.java
@@ -5,10 +5,18 @@ public class demoWrapper {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i=10;
-		Integer i1=new Integer(i);//here convert primitive to object datatype
+		Integer i1=new Integer(i);//here convert primitive to object data type
 		System.out.println(i1);//in co
-		int i3=i1;
-		System.out.println(i3);
+		//int i3=i1;//here atuomatically conevert from object data type to primitive data type
+		//or
+		int i3= i1.intValue();//here also convert object dt to primitive data  type
+		System.out.println(i3);//here convert 
+		
+		float f=10.23f;
+		Float f1= new Float(f);//here convert primitive data type  to object data type
+		System.out.println(f);
+		float f2=f1.floatValue();//conevert from object data type to primitive data type
+		
 		
 	}
 
